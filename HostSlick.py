b7a13d6318fc7ffed6c51b9bf9ee8be4378368ca
SlickMercy <135204260+SlickxMercy@users.noreply.github.com>
2023-06-07 23:13:41 -0600

    Add files via upload

--- HostSlick.py
+++ HostSlick.py
@@ -1,3 +1,11 @@
+"""
+Nombre del archivo: HostSlick.py
+Autor: SlickMercy
+Fecha de creación: 1 de junio de 2023
+Descripción: TG: SlickMercy.
+
+Este código está sujeto a derechos de autor. Todos los derechos están reservados.
+"""
 import os
 import re
 import sys
@@ -17,40 +25,46 @@ def scan_ips(start_ip, end_ip):
                     ips.append(ip)
     return ips
 
-def test_ip(ip):
+def test_ip(ip, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(2)
-        sock.connect((ip, 80))
+        sock.connect((ip, port))
         sock.close()
         return True
     except:
         return False
 
-def find_cameras(start_ip, end_ip, filename):
+def find_cameras(start_ip, end_ip, port):
     ips = scan_ips(start_ip, end_ip)
-    
+    total_ips = len(ips)
+    scanned_ips = 0
+    saved_ips = 0
+
     class CameraScanner(threading.Thread):
-        def __init__(self, ip_list, filename):
+        def __init__(self, ip_list):
             threading.Thread.__init__(self)
             self.ip_list = ip_list
-            self.filename = filename
 
         def run(self):
+            nonlocal scanned_ips, saved_ips
             for ip in self.ip_list:
-                if test_ip(ip):
-                    with open(self.filename, "a+") as f:
+                if test_ip(ip, port):
+                    with open("host.txt", "a+") as f:
                         existing_ips = set(f.read().splitlines())
                         if ip not in existing_ips:
                             f.write(f"{ip}\n")
-                            print(f"\r\033[32mAdded {ip} to {self.filename}\033[0m", end='', flush=True)
+                            saved_ips += 1
+                scanned_ips += 1
+                progress = (scanned_ips / total_ips) * 100
+                print(f"\rProgress: {progress:.2f}% | Saved IPs: {saved_ips}", end='', flush=True)
 
     threads = []
     num_threads = 200
     chunk_size = len(ips) // num_threads
     for i in range(num_threads):
         thread_ips = ips[i*chunk_size:(i+1)*chunk_size]
-        thread = CameraScanner(thread_ips, filename)
+        thread = CameraScanner(thread_ips)
         threads.append(thread)
         thread.start()
 
@@ -62,5 +76,5 @@ def find_cameras(start_ip, end_ip, filename):
 if __name__ == '__main__':
     start_ip = input("Enter the start IP address range: ")
     end_ip = input("Enter the end IP address range: ")
-    filename = input("Enter the filename to save the IP addresses: ")
-    find_cameras(start_ip, end_ip, filename)
+    port = input("Enter the port to scan: ")
+    find_cameras(start_ip, end_ip, int(port))
